8cf8ac76e24be08ecd74d887592f0e5d65851f11
andrefv3 <andresfelipevega567@gmail.com>
2024-01-10 20:23:16 -0500

    Added configs for Apollo/GraphQL

--- src/main.tsx
+++ src/main.tsx
@@ -7,30 +7,50 @@ import { store, persistor } from './store.ts'
 import { PersistGate } from 'redux-persist/integration/react'
 import { WishlistProvider } from './contexts/WishlistContext.tsx';
 import { CartProvider } from './contexts/CartContext.tsx';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-     <PersistGate persistor={persistor}>
-      <React.StrictMode>
-        <BrowserRouter>
-        <CartProvider>
-          <WishlistProvider>
-            <Routes>
-              {routes.map((value) => {
-                return (
-                  <Route 
-                    key={value.key} 
-                    path={value.path}
-                    element={value.component}>
-                  </Route>
-                )
-              })}
-            </Routes>
-          </WishlistProvider>
-        </CartProvider>
-        </BrowserRouter>
-      </React.StrictMode>
-     </PersistGate>
-  </Provider>
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+)
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: import.meta.env.VITE_API_REST_SHOPI,
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'no-cache',
+    },
+    query: {
+      fetchPolicy: 'no-cache',
+    },
+  }
+})
+
+root.render(
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <React.StrictMode>
+          <BrowserRouter>
+          <CartProvider>
+            <WishlistProvider>
+              <Routes>
+                {routes.map((value) => {
+                  return (
+                    <Route 
+                      key={value.key} 
+                      path={value.path}
+                      element={value.component}>
+                    </Route>
+                  )
+                })}
+              </Routes>
+            </WishlistProvider>
+          </CartProvider>
+          </BrowserRouter>
+        </React.StrictMode>
+      </PersistGate>
+    </Provider>
+  </ApolloProvider>
 )
